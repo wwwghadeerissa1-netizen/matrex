d2ac9723044a8bd42aec25d60924aa2713e4d248
wwwghadeerissa1-netizen <www.ghadeerissa1@gmail.com>
2026-09-12 01:16:48 +0300

    Bump Matrex Android app to Pro 2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.matrex.repair"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "2.0.0"
     }
 
     compileOptions {
