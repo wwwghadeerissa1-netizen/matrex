68a6c469d22107687d086f760ed3d785a0024c99
wwwghadeerissa1-netizen <www.ghadeerissa1@gmail.com>
2026-09-12 00:57:54 +0300

    Add Matrex Android application module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android { namespace = "com.matrex.repair"; compileSdk = 35
+    defaultConfig { applicationId = "com.matrex.repair"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+}
